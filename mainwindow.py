d117acde888a2bc461d2a1dafdfcfc029c0ec386
CraftBox <i.domnich2017@gmail.com>
2024-03-25 20:47:36 +0200

    - Added a visual interface for work with product categories.

--- mainwindow.py
+++ mainwindow.py
@@ -2,7 +2,7 @@ from PySide6 import QtGui, QtCore
 from PySide6.QtCore import Qt
 from PySide6.QtCore import QSortFilterProxyModel
 from PySide6.QtGui import QStandardItemModel, QStandardItem
-from PySide6.QtWidgets import QMainWindow, QFileDialog
+from PySide6.QtWidgets import QMainWindow, QFileDialog, QAbstractItemView
 from bs4 import BeautifulSoup
 from ui_mainwindow import Ui_MainWindow
 
@@ -14,8 +14,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.app = app
 
         # Init of table_view_1
-        self.source_cat_model = QStandardItemModel()
         header_name = ["Категорія"]
+        self.source_cat_model = QStandardItemModel()
         self.source_cat_model.setHorizontalHeaderLabels(header_name)
         self.source_cat_proxy_model = QSortFilterProxyModel()
         self.source_cat_proxy_model.setSourceModel(self.source_cat_model)
@@ -34,25 +34,101 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.final_category_table_view.horizontalHeader().setDefaultAlignment(Qt.AlignLeft)
 
         self.search_line_edit.textChanged.connect(self.onTextChanged)
-
         self.open_action.triggered.connect(self.open_file)
+        self.add_cat_push_button.clicked.connect(self.add_category)
+        self.delete_cat_push_button.clicked.connect(self.remove_category)
+
         self.xml_data = None
 
     def onTextChanged(self, text):
         self.source_cat_proxy_model.setFilterFixedString(text)
 
+    def add_category(self):
+        sctv_model = self.source_category_table_view.model()
+        fctv_model = self.final_category_table_view.model()
+        sctv_results = []
+        # Select checked categories to the final category table
+        # for row in range(row_count):
+        row_count = sctv_model.rowCount()
+        row = 0
+        while True:
+            checked = sctv_model.data(sctv_model.index(row, 0), Qt.CheckStateRole)
+            item = sctv_model.data(sctv_model.index(row, 0))
+            if checked == 2:
+                sctv_results.append(item)
+                sctv_model.removeRow(row)
+            else:
+                row += 1
+            if row >= row_count:
+                break
+
+        print(sctv_results)
+
+        # Checking for the presence of an element in the final table
+        fctv_results = []
+        for row in range(fctv_model.rowCount()):
+            item = fctv_model.data(fctv_model.index(row, 0))
+            fctv_results.append(item)
+
+        for category in sctv_results:
+            category_item = QStandardItem()
+            category_item.setData(category, Qt.DisplayRole)
+            category_item.setCheckable(True)
+            # If the element is not in the final table, add it
+            if category_item.data(Qt.DisplayRole) not in fctv_results:
+                self.fin_cat_model.appendRow(category_item)
+
+    def remove_category(self):
+        fctv_model = self.source_category_table_view.model()
+        sctv_model = self.final_category_table_view.model()
+        sctv_results = []
+        # Select checked categories to the final category table
+        # for row in range(row_count):
+        row_count = sctv_model.rowCount()
+        row = 0
+        while True:
+            checked = sctv_model.data(sctv_model.index(row, 0), Qt.CheckStateRole)
+            item = sctv_model.data(sctv_model.index(row, 0))
+            if checked == 2:
+                sctv_results.append(item)
+                sctv_model.removeRow(row)
+            else:
+                row += 1
+            if row >= row_count:
+                break
+
+        print(sctv_results)
+
+        # Checking for the presence of an element in the final table
+        fctv_results = []
+        for row in range(fctv_model.rowCount()):
+            item = fctv_model.data(fctv_model.index(row, 0))
+            fctv_results.append(item)
+
+        for category in sctv_results:
+            category_item = QStandardItem()
+            category_item.setData(category, Qt.DisplayRole)
+            category_item.setCheckable(True)
+            # If the element is not in the final table, add it
+            if category_item.data(Qt.DisplayRole) not in fctv_results:
+                self.source_cat_model.insertRow(0, category_item)
+
     # open xml file
     def open_file(self):
         file_path, _ = QFileDialog.getOpenFileName(filter="XML Files (*.xml)")
         categories = self.parse(file_path)
-        for category in categories:
-            category_item = QStandardItem(category)
-            category_item.setCheckable(True)
-            self.source_cat_model.appendRow(category_item)
-
+        self.populate_source_cat_table(categories)
         print("File closed")
 
-    def parse(self, file_path):
+    def populate_source_cat_table(self, categories):
+        for category in categories:
+            category_item = QStandardItem()
+            category_item.setCheckable(True)
+            category_item.setData(category, Qt.DisplayRole)
+            self.source_cat_model.appendRow(category_item)
+
+    @staticmethod
+    def parse(file_path):
         if file_path:
             # Завантажити файл
             with open(file_path, "r", encoding="windows-1251") as f:
--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -19,13 +19,13 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
 from PySide6.QtWidgets import (QApplication, QHBoxLayout, QHeaderView, QLabel,
     QLineEdit, QMainWindow, QMenu, QMenuBar,
     QPushButton, QSizePolicy, QStatusBar, QTableView,
-    QWidget)
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(666, 214)
+        MainWindow.resize(666, 253)
         self.open_action = QAction(MainWindow)
         self.open_action.setObjectName(u"open_action")
         self.exit_action = QAction(MainWindow)
@@ -34,22 +34,22 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(10, 140, 91, 23))
+        self.pushButton.setGeometry(QRect(10, 180, 91, 23))
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(10, 0, 111, 16))
         self.label_2 = QLabel(self.centralwidget)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(360, 0, 91, 16))
+        self.label_2.setGeometry(QRect(356, 0, 100, 16))
         self.search_line_edit = QLineEdit(self.centralwidget)
         self.search_line_edit.setObjectName(u"search_line_edit")
-        self.search_line_edit.setGeometry(QRect(58, 37, 113, 20))
+        self.search_line_edit.setGeometry(QRect(60, 37, 201, 20))
         self.label_3 = QLabel(self.centralwidget)
         self.label_3.setObjectName(u"label_3")
         self.label_3.setGeometry(QRect(14, 40, 47, 13))
         self.widget = QWidget(self.centralwidget)
         self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(10, 60, 601, 73))
+        self.widget.setGeometry(QRect(10, 60, 601, 111))
         self.horizontalLayout = QHBoxLayout(self.widget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
@@ -58,10 +58,20 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout.addWidget(self.source_category_table_view)
 
-        self.pushButton_2 = QPushButton(self.widget)
-        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.add_cat_push_button = QPushButton(self.widget)
+        self.add_cat_push_button.setObjectName(u"add_cat_push_button")
 
-        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.verticalLayout.addWidget(self.add_cat_push_button)
+
+        self.delete_cat_push_button = QPushButton(self.widget)
+        self.delete_cat_push_button.setObjectName(u"delete_cat_push_button")
+
+        self.verticalLayout.addWidget(self.delete_cat_push_button)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
 
         self.final_category_table_view = QTableView(self.widget)
         self.final_category_table_view.setObjectName(u"final_category_table_view")
@@ -97,7 +107,9 @@ class Ui_MainWindow(object):
         self.label.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u0447\u0430\u0442\u043a\u043e\u0432\u0438\u0439 \u0441\u043f\u0438\u0441\u043e\u043a", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u041a\u0456\u043d\u0446\u0435\u0432\u0438\u0439 \u0441\u043f\u0438\u0441\u043e\u043a", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u0448\u0443\u043a:", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0434\u0430\u0442\u0438 \u0434\u043e\n"
+        self.add_cat_push_button.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0434\u0430\u0442\u0438 \u0434\u043e\n"
+"XML", None))
+        self.delete_cat_push_button.setText(QCoreApplication.translate("MainWindow", u"\u0423\u0431\u0440\u0430\u0442\u0438 \u0437\n"
 "XML", None))
         self.menu.setTitle(QCoreApplication.translate("MainWindow", u"\u0424\u0430\u0439\u043b", None))
     # retranslateUi
